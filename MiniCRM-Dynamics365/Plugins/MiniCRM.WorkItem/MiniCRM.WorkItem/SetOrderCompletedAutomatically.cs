a12585f4d4ef569b4d5d21da5ac45e9bfd562bb7
petrhruby7 <hruby.petr.7@gmail.com>
2025-09-08 18:05:18 +0200

    refactored Plugin MiniCRM.WorkItem

--- MiniCRM-Dynamics365/Plugins/MiniCRM.WorkItem/MiniCRM.WorkItem/SetOrderCompletedAutomatically.cs
+++ MiniCRM-Dynamics365/Plugins/MiniCRM.WorkItem/MiniCRM.WorkItem/SetOrderCompletedAutomatically.cs
@@ -1,92 +1,120 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 
 namespace MiniCRM.WorkItem
 {
+    /// <summary>
+    /// Plugin that automatically sets an order to "Completed" status when all related work items are completed
+    /// </summary>
     public class SetOrderCompletedAutomatically : IPlugin
     {
-        private ITracingService tracer;
+
         private IPluginExecutionContext context;
         private IOrganizationServiceFactory serviceFactory;
         private IOrganizationService service;
         private Entity contextEntity;
-        private Entity FullEntity;
-        private Guid InitiatingUserID;
         public void Execute(IServiceProvider serviceProvider)
         {
-            tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
             context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
             serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
             service = serviceFactory.CreateOrganizationService(context.UserId);
+            
             if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
             {
                 contextEntity = (Entity)context.InputParameters["Target"];
-                InitiatingUserID = context.InitiatingUserId;
             }
 
             try
             {
-                if (context.MessageName.ToLower() == "update" && contextEntity.LogicalName.ToLower() == "new_workitem" && contextEntity.Contains("new_workitemstatus"))
+                if (context.MessageName.ToLower() == "update" 
+                    && contextEntity.LogicalName.ToLower() == "new_workitem" 
+                    && contextEntity.Contains("new_workitemstatus")
+                    && context.PreEntityImages.Contains("PreImage"))
                 {
-                    tracer.Trace("NEED REFACTORING AND TESTING");
-
-
+                    // Get work item status, ID and related order
                     OptionSetValue new_workitemstatus = contextEntity.GetAttributeValue<OptionSetValue>("new_workitemstatus");
-                    Guid new_workitemid = contextEntity.GetAttributeValue<Guid>("new_workitemid");
+                    Guid new_workitemid = contextEntity.Id;
+
+                    Entity preImage = context.PreEntityImages["PreImage"];
+                    EntityReference new_order = preImage.GetAttributeValue<EntityReference>("new_order");
 
                     // Check all regarded work items if they are all completed (100000002)
-                    if (new_workitemstatus.Value == 100000002)
+                    if (new_workitemstatus.Value == 100000002 && new_order != null)
                     {
-                        //Get regarded order
-                        EntityReference new_order = contextEntity.GetAttributeValue<EntityReference>("new_order");
+                        // Get all other work items for the same order
+                        EntityCollection relatedWorkItems = GetOtherWorkItemsForOrder(new_order.Id, new_workitemid);
 
-                        var query = new QueryExpression("new_workitem")
+                        if (relatedWorkItems != null && relatedWorkItems.Entities.Count >= 0)
                         {
-                            ColumnSet = new ColumnSet("new_workitemstatus"),
-                            Criteria = new FilterExpression
-                            {
-                                Conditions =
-                                {
-                                    new ConditionExpression("new_order", ConditionOperator.Equal, new_order),
-                                    new ConditionExpression("new_workitemid", ConditionOperator.NotEqual, new_workitemid)
-                                }
-                            }
-                        };
-                        
-                        EntityCollection regardedWorkItems = service.RetrieveMultiple(query);
+                            // Check if all other work items are completed
+                            bool shouldUpdateOrder = IsEveryRegardedItemCompleted(relatedWorkItems);
 
-
-                        if (regardedWorkItems != null && regardedWorkItems.Entities.Count >= 0)
-                        {
-                            int sumOfCompletedWorkItems = 0;
-                            foreach (Entity workItem in regardedWorkItems.Entities)
+                            if (shouldUpdateOrder)
                             {
-                                OptionSetValue regardedWorkItemStatus = workItem.GetAttributeValue<OptionSetValue>("new_workitemstatus");
-                                if (regardedWorkItemStatus.Value == 100000002)
-                                {
-                                    sumOfCompletedWorkItems++;
-                                }
+                                Entity relatedOrder = service.Retrieve("new_order", new_order.Id, new ColumnSet("new_orderstatus"));
+                                relatedOrder["new_orderstatus"] = new OptionSetValue(100000004); //Completed
+                                service.Update(relatedOrder);
                             }
-
-                            if (sumOfCompletedWorkItems == regardedWorkItems.Entities.Count)
-                            {
-                                Entity regardedOrder = service.Retrieve("new_order", new_order.Id, new ColumnSet("new_orderstatus"));
-                                regardedOrder["new_orderstatus"] = 100000004; //Completed
-                            }
-                        } 
+                        }
                     }
                 }
-
-
-            } catch (Exception ex)
-            {
-                throw new InvalidPluginExecutionException("Can't set order status to completed", ex);
             }
+            catch (Exception ex)
+            {
+
+                throw new InvalidPluginExecutionException("Error setting order status to completed", ex);
+            }
+        }
+
+        /// <summary>
+        /// Checks if all work items in the collection are completed
+        /// </summary>
+        /// <param name="relatedWorkItems">Collection of work items to check</param>
+        /// <returns>True if all work items are completed, false otherwise</returns>
+        private bool IsEveryRegardedItemCompleted(EntityCollection relatedWorkItems)
+        {
+            bool allCompleted = true;
+            // Iterate through all work items and check their status
+            foreach (Entity workItem in relatedWorkItems.Entities)
+            {
+                OptionSetValue regardedWorkItemStatus = workItem.GetAttributeValue<OptionSetValue>("new_workitemstatus");
+
+                if (regardedWorkItemStatus.Value != 100000002)
+                {
+                    allCompleted = false;
+                    break; // First non-completed found, stop checking
+                }
+            }
+            return allCompleted;
+        }
+
+        /// <summary>
+        /// Retrieves all other work items for a specific order, excluding the current work item
+        /// </summary>
+        /// <param name="orderId">ID of the order</param>
+        /// <param name="currentWorkItemId">ID of the current work item to exclude</param>
+        /// <returns>Collection of other work items for the order</returns>
+        private EntityCollection GetOtherWorkItemsForOrder(Guid orderId, Guid currentWorkItemId)
+        {
+            // Build query to get all work items for the order except the current one
+            var query = new QueryExpression("new_workitem")
+            {
+                ColumnSet = new ColumnSet("new_workitemstatus"),
+                Criteria = new FilterExpression
+                {
+                    Conditions =
+            {
+                new ConditionExpression("new_order", ConditionOperator.Equal, orderId),
+                new ConditionExpression("new_workitemid", ConditionOperator.NotEqual, currentWorkItemId),
+                new ConditionExpression("statecode", ConditionOperator.Equal, 0)
+            }
+                }
+            };
+            
+            return service.RetrieveMultiple(query);
         }
     }
 }
+
